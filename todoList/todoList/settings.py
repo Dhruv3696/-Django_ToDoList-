005d0949726e35dffba9ac86650addbc33881e0e
Dhruv3696 <64467268+Dhruv3696@users.noreply.github.com>
2023-05-12 15:43:56 +0530

    Commit-3
    Database Integration with the application

--- todoList/todoList/settings.py
+++ todoList/todoList/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ToDoDatabase',
-        'USER': 'admin',
+        'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': '5432',
